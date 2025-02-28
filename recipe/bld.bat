ddaabc6bd509f1de7dcef978bff2fdaa0ee55ca7
Andrii Osipov <aosipov@anaconda.com>
2025-02-28 14:07:55 -0800

    fix win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -16,9 +16,11 @@ attrib -R /S /D %SRC_DIR%
 attrib -R /S /D %BUILD_PREFIX%
 
 :: Now shut down Bazel server, otherwise Windows would not allow moving a directory with it
-@for %%G in  ("%SRC_DIR%") DO @SET DRIVE=%%~dG
-bazel "--output_base=%DRIVE%\O" clean --expunge
-bazel "--output_base=%DRIVE%\O" shutdown
+::@for %%G in  ("%SRC_DIR%") DO @SET DRIVE=%%~dG
+::bazel "--output_base=%DRIVE%\O" clean --expunge
+::bazel "--output_base=%DRIVE%\O" shutdown
+bazel clean --expunge
+bazel shutdown
 
 :: WARNING conda.gateways.disk.delete:unlink_or_rename_to_trash(184): Could not remove or rename _build_env\Library\lib\modules.  Please remove this file manually (you may need to reboot to free file handles)
 rmdir /S "%BUILD_PREFIX%\Library\lib\modules"
