1c7f7e27e05e8bf4921b2bded6986a4035468d0f
Andrii Osipov <aosipov@anaconda.com>
2025-02-28 12:40:12 -0800

    added chmod +x on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,3 +9,9 @@ set PYTHON_LIB_PATH=%SP_DIR%
 :: Now shut down Bazel server, otherwise Windows would not allow moving a directory with it
 bazel clean --expunge
 bazel shutdown
+
+:: bazel by default makes everything read-only
+:: removing readonly from all files and directories
+attrib -R /S /D %PREFIX%
+attrib -R /S /D %SRC_DIR%
+attrib -R /S /D %BUILD_PREFIX%
