23cc81e954faf70ac443acaa6273b06166344274
Lorenzo Pirritano <lpirritano@anaconda.com>
2025-03-03 22:12:31 +0100

    set python version for win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,12 +6,15 @@ bazel shutdown
 set "BAZEL_VC=%VSINSTALLDIR%VC"
 set "BAZEL_VS=%VSINSTALLDIR%"
 
+echo Using Python: %PYTHON%
+%PYTHON% --version
+
 :: Python settings
-set PYTHON_BIN_PATH=%PYTHON%
-set PYTHON_LIB_PATH=%SP_DIR%
+set PYTHON_BIN_PATH="%PYTHON%"
+set PYTHON_LIB_PATH="%SP_DIR%"
+set PYTHON_VERSION=%PY_VER%
 
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
-
 
 :: Now shut down Bazel server, otherwise Windows would not allow moving a directory with it
 bazel clean --expunge
