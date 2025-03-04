84e05b8ec8bed46ad0a40208b3cc660a31cdbe79
Andrii Osipov <aosipov@anaconda.com>
2025-03-04 13:11:30 -0800

    setup custom sdk for osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,4 +6,9 @@ rm -rf ~/^.cache/bazel/
 export PYTHON_BIN_PATH="$PYTHON"
 export PYTHON_LIB_PATH="$SP_DIR"
 
+if [[ $target_platform == osx-* ]]; then
+    ls -la /Library/Developer/CommandLineTools/SDKs/*.sdk
+    ls -la /opt/*.sdk
+fi
+
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
